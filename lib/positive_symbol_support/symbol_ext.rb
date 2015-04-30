f2c85c6208743c3f6db836afd1d260198a09d8c6
Shu Fujita <osorubeki.fujita@gmail.com>
2015-05-01 00:32:29 +0900

    20150501_003228 Install Gem (0.1.0)

--- lib/positive_symbol_support/symbol_ext.rb
+++ lib/positive_symbol_support/symbol_ext.rb
@@ -1,11 +1,45 @@
+# Symbol に機能を追加するためのモジュール
+# @see http://api.rubyonrails.org/classes/ActiveSupport/Inflector.html
 module PositiveSymbolSupport::SymbolExt
 
-  [ :underscore , :camelize , :singularize , :pluralize ].each do | method_name |
+  [ :underscore , :camelize , :singularize , :pluralize ,
+    :classify , :dasherize ,
+    :tableize , :titleize , :titlecase
+  ].each do | method_name |
     eval <<-DEF
       def #{method_name.to_s}
         self.to_s.#{method_name.to_s}.to_sym
       end
     DEF
+  end
+
+  def constantize
+    self
+  end
+
+  [ :deconstantize , :demodulize ].each do | instance_method_name |
+    eval <<-DEF
+      def #{instance_method_name}
+        str = to_s.send( __method__ )
+        if str.blank?
+          nil
+        else
+          str.to_sym
+        end
+      end
+    DEF
+  end
+
+  def foreign_key( separate_class_name_and_id_with_underscore = true )
+    to_s.send( __method__ , separate_class_name_and_id_with_underscore ).to_sym
+  end
+
+  def humanize( options = {} )
+    to_s.send( __method__ , options ).to_sym
+  end
+
+  def parameterize( sep = "-" )
+    to_s.send( __method__ , sep ).to_sym
   end
 
 end
